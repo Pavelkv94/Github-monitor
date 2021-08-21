895378d943b2e211845e3322d143683e7fdf48f5
Pavelkv94 <pavel.kv94@yandex.by>
2021-08-21 18:13:40 +0300

    feat: get users from search

--- src/components/Github/Github.tsx
+++ src/components/Github/Github.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import s from './Github.module.css';
 
+type SearchUserType = {
+    login: string
+    id: number
+}
+type SearchResult = {
+    items: SearchUserType[]
+}
+
 export function Github() {
+    const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
+    const [users, setUsers] = useState<SearchUserType[]>([])
+    const [tempSearch, setTempSearch] = useState<string>("")
+
+    useEffect(() => {
+        if (selectedUser) {
+            document.title = selectedUser.login
+        }
+    }, [selectedUser])
+
+    useEffect(() => {
+        axios.get<SearchResult>('https://api.github.com/search/users?q=')
+            .then(res => {
+                setUsers(res.data.items)
+            })
+    }, [])
+
     return (<div className={s.container}>
         <div className={s.searchContainer}>
             <div>
-                <input type="text" placeholder="search" /> <button>Find</button>
+                <input
+                 type="text" 
+                placeholder="search" 
+                value={tempSearch} 
+                onChange={(e)=>{setTempSearch(e.currentTarget.value)}}
+                /> 
+                <button
+                onClick={()=>{
+                    axios.get<SearchResult>('https://api.github.com/search/users?q=' + tempSearch)
+                    .then(res => {
+                        setUsers(res.data.items)
+                    })
+                }}
+                >
+                    Find
+                    </button>
             </div>
             <div>
                 <ul>
-                    {['Anna', 'Artem'].map(u => <li>{u}</li>)}
+                    {users.map(u =>
+                        <li key={u.id}
+                            onClick={() => {
+                                setSelectedUser(u);
+                                //document.title = u
+                            }}
+                            className={selectedUser === u ? s.selected : ""}
+
+                        >{u.login}</li>)
+                    }
                 </ul>
             </div>
         </div>
